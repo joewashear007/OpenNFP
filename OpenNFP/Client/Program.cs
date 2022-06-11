e1ab9277dad7d665644e1492f7587977ee6447ab
Joseph Livecchi <joewashear007@gmail.com>
2022-06-10 21:38:32 -0400

    Added Local Storage Service

--- OpenNFP/Client/Program.cs
+++ OpenNFP/Client/Program.cs
@@ -4,6 +4,8 @@ using OpenNFP.Client;
 using MudBlazor.Services;
 using OpenNFP.Shared;
 using OpenNFP.Shared.Models;
+using Blazored.LocalStorage;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,6 +16,7 @@ builder.Services.AddMudServices();
 builder.Services.AddSingleton<IChartingRepo, ChartingRepo>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 
 
