35578d41e4df08120692b0ae15c094be80776d91
Joseph Livecchi <jlivecchi@servicetitan.com>
2022-12-22 16:43:00 -0500

    Test using using custom copy of auth service

--- OpenNFP/Client/Program.cs
+++ OpenNFP/Client/Program.cs
@@ -22,7 +22,6 @@ builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Local", options.ProviderOptions);
     options.ProviderOptions.DefaultScopes.Add("email");
-    //options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/drive.file");
 });
 
 
