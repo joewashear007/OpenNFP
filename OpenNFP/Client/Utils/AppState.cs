c067bfa7a7c4b96c7283902043f7309e7a436858
Joseph Livecchi <jlivecchi@servicetitan.com>
2023-02-16 11:15:21 -0500

    Updated list of cycles to load all but not the charts

--- OpenNFP/Client/Utils/AppState.cs
+++ OpenNFP/Client/Utils/AppState.cs
@@ -8,11 +8,21 @@
         public bool ShouldRefresh { get; set; } = true;
 
         public bool ShouldRunSyncOnStartup { get; set; } = true;
+
+        public int InitialCyclesToLoad { get; set; } = 3;
     }
 
     public interface IAppState
     {
+        /// <summary>
+        /// Run the sync on app startup
+        /// </summary>
         bool ShouldRunSyncOnStartup { get; set; }
+
+        /// <summary>
+        /// The number of cycles to load on the cycle list screen
+        /// </summary>
+        int InitialCyclesToLoad { get; set; }
         
     }
 }
