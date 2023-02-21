e6e87731c522d4bc93864cf3f45e0591c9d16dec
Joseph Livecchi <jlivecchi@servicetitan.com>
2023-02-20 22:55:59 -0500

    Tweaks to edit day form

---
+++ OpenNFP/Client/Utils/EnumSliderHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace OpenNFP.Client.Utils
+{
+    public class EnumSliderHelper<T> where T : struct, Enum
+    {
+        public EnumSliderHelper(Action<T> setProp)
+        {
+            this.setProp = setProp;
+        }
+        private T _value = default;
+        private readonly Action<T> setProp;
+
+        public string[] Labels => Enum.GetNames<T>();
+        public int Min => Enum.GetValues<T>().Cast<int>().Min();
+        public int Max => Enum.GetValues<T>().Cast<int>().Max();
+        public int ValueInt
+        {
+            get => (int)(object)_value;
+            set
+            {
+                T newValue = (T)(object)value;
+                _value = newValue;
+                setProp(newValue);
+            }
+        }
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                setProp(value);
+            }
+        }
+    }
+}
