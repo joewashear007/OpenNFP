23fe6bc503768f140833135636c498af506bff50
Joseph Livecchi <jlivecchi@servicetitan.com>
2024-08-28 23:19:27 -0400

    Update to net8

--- OpenNFP/Shared/Extensions.cs
+++ OpenNFP/Shared/Extensions.cs
@@ -9,6 +9,31 @@ namespace OpenNFP.Shared
 {
     public static class Extensions
     {
+        /// <summary>
+        /// https://stackoverflow.com/a/33104162
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="items"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            if (list is List<T> asList)
+            {
+                asList.AddRange(items);
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    list.Add(item);
+                }
+            }
+        }
+
         public static string ToKey(this DateTime date)
         {
             return date.ToString("yyyyMMdd");
